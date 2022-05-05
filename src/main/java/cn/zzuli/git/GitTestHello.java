7d397004031f959be3cf4206b9eac50d89747410
simpleJun <3061406488@qq.com>
2022-05-06 07:49:54 +0800

    success  2

--- src/main/java/cn/zzuli/git/GitTestHello.java
+++ src/main/java/cn/zzuli/git/GitTestHello.java
@@ -3,5 +3,6 @@ package cn.zzuli.git;
 public class GitTestHello {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git2");
     }
 }
